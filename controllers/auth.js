2a7e8e4f3718f08f0db091d6882a57c203e236eb
nidhi <nidhi7485@gmail.com>
2022-10-22 00:45:04 +0530

    user schema created

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const getAllJobs = async (req, res) => {
+  res.send('get all jobs')
+}
+
+const getSingleJobs = async (req, res) => {
+  res.send('get jobs')
+}
+const createJobs = async (req, res) => {
+  res.send('create jobs')
+}
+const getUpdateJobs = async (req, res) => {
+  res.send('update jobs')
+}
+const getDeleteJobs = async (req, res) => {
+  res.send('Delete jobs')
+}
+module.exports = {}
