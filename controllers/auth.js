44ab915858bbc786d0ad2fb23e394930738acf4d
nidhi <nidhi7485@gmail.com>
2022-10-23 17:58:23 +0530

    token generated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,19 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const bcrypt = require('bcryptjs')
+
+const { BadRequestError } = require('../errors')
 const logIn = async (req, res) => {
   res.send('login page')
 }
 
 const register = async (req, res) => {
   // console.log(req.body)
+
   const user = await User.create(req.body)
-  console.log(user)
-  res.status(StatusCodes.CREATED).json({ user })
+  // console.log(user)
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 module.exports = {
   logIn,
