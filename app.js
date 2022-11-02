cdeb3af3742d88a6c90bf15233d11072014ebc52
nidhi <nidhi7485@gmail.com>
2022-11-02 11:45:39 +0530

    complete all setup

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const ratelimiter = require('express-rate-limit')
+
 //DB
 const connectDB = require('./db/connect')
 
@@ -16,6 +21,17 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
+
+app.set('trust proxy', 1)
+app.use(
+  ratelimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
